0f629c4a6e4227361df9b3adc88d63a758269159
szlj07 <matt.rippin@gm.com>
2017-03-27 22:10:49 -0400

    MR | update to mainactivity for communication with facebook. Added permissions perams

--- MyApplication2/app/src/main/java/com/in/ripp/facebooktest/MainActivity.java
+++ MyApplication2/app/src/main/java/com/in/ripp/facebooktest/MainActivity.java
@@ -1,59 +1,80 @@
 package com.in.ripp.facebooktest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "Rip";
+    public CallbackManager callbackManager;
+    public AccessToken accessToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        final LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
+        loginButton.setReadPermissions("public_profile");
+        loginButton.setReadPermissions("user_friends");
 
-//
-//        CallbackManager callbackManager = new CallbackManager() {
-//
-//            @Override
-//            public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-//                Log.d(TAG, "onActivityResult: "+requestCode+":"+resultCode+":"+ data.toString());
-//                return false;
-//            }
-//        };
-        // Callback registration
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                // App code
-                Log.d(TAG, "onSuccess: facebook success");
-            }
-
-            @Override
-            public void onCancel() {
-                // App code
-                Log.d(TAG, "onCancel: cancel success");
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-                Log.d(TAG, "onError: error success");
-            }
-        });
+        callbackManager = CallbackManager.Factory.create();
 
 
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.d(TAG, "onSuccess: facebook success from login manager");
+                        accessToken = loginResult.getAccessToken();
+                        /* make the API call */
+                        new GraphRequest(
+                                AccessToken.getCurrentAccessToken(),
+                                "/me/taggable_friends",
+                                null,
+                                HttpMethod.GET,
+                                new GraphRequest.Callback() {
+                                    public void onCompleted(GraphResponse response) {
+                                    /* handle the result */
+                                        Log.d(TAG, "onCompleted: " + response.toString());
+                                    }
+                                }
+                        ).executeAsync();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d(TAG, "onCancel: facebook oncancel from Login Manager");
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.d(TAG, "onError: onerror from Login manager");
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
 
